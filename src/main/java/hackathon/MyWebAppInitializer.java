dae9110ffe0fc7086b045072631f2d529b2d15d5
Dmitriy <dmitriy.sev.egorov@gmail.com>
2019-05-29 22:25:50 +0300

    WebApplicationInitializer

---
+++ src/main/java/hackathon/MyWebAppInitializer.java
@@ -0,0 +1,31 @@
+package hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * @author Dmitriy
+ * @since 29.05.2019
+ */
+public class MyWebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext container) {
+        AnnotationConfigWebApplicationContext context
+                = new AnnotationConfigWebApplicationContext();
+        context.register(Application.class);
+
+        container.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher = container
+                .addServlet("dispatcher", new DispatcherServlet(context));
+
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
