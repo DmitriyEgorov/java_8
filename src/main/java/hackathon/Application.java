3dfb770b3aaf2acbe622ef89a591c0f9ea3bbca2
Dmitriy <dmitriy.sev.egorov@gmail.com>
2019-05-29 21:46:49 +0300

    web_application

---
+++ src/main/java/hackathon/Application.java
@@ -0,0 +1,17 @@
+package hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dmitriy
+ * @since 29.05.2019
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/hackathon/TestController.java
+++ src/main/java/hackathon/TestController.java
@@ -1,4 +1,17 @@
 package hackathon;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dmitriy
+ * @since 28.05.2019
+ */
+@RestController
 public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "test success";
+    }
 }
