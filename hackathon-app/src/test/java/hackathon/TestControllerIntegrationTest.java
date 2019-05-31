b0a64620a6c3791b8b7d2ff1d598f0a93570f99f
Dmitriy <dmitriy.sev.egorov@gmail.com>
2019-05-31 22:34:12 +0300

    Integration_test+configuration

---
+++ hackathon-app/src/test/java/hackathon/TestControllerIntegrationTest.java
@@ -0,0 +1,52 @@
+package hackathon;
+
+import hackathon.model.TestResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Integration test for TestController
+ *
+ * @author Dmitriy
+ * @since 31.05.2019
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHi() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Dmitriy, Mickhail and Sergey say hi!")));
+    }
+
+    @Test
+    public void testPing() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/ping")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(mvcResult ->
+                        assertTrue(mvcResult.getResponse().getContentAsString().contains("test success")));
+
+    }
+
+}
